6310fbdeb8950bf771d93a66955023fc3d6cfa0c
cuirenx-dot <cuirenx@gmail.com>
2026-08-21 16:45:51 +0900

    Update maps.js

--- js/maps.js
+++ js/maps.js
@@ -1,1 +1,40 @@
+const MAPS = {
 
+station_day1: {
+
+    name: "駅",
+
+    objects: [
+
+        {
+            id:"yui",
+            icon:"👧",
+            x:220,
+            y:100
+        },
+
+        {
+            id:"ticket",
+            icon:"🎫",
+            x:250,
+            y:240
+        },
+
+        {
+            id:"bench",
+            icon:"🪑",
+            x:50,
+            y:220
+        },
+
+        {
+            id:"clock",
+            icon:"🕒",
+            x:20,
+            y:20
+        }
+
+    ]
+}
+
+};
