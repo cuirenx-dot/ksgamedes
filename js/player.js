91c17ab40a788d9aa3455d8b6881dc5ce5ae5c17
cuirenx-dot <cuirenx@gmail.com>
2026-08-21 16:46:13 +0900

    Update player.js

--- js/player.js
+++ js/player.js
@@ -1,1 +1,38 @@
+let playerX = 140;
+let playerY = 140;
 
+function move(dx,dy){
+
+    playerX += dx * 40;
+    playerY += dy * 40;
+
+    if(playerX < 0) playerX = 0;
+    if(playerY < 0) playerY = 0;
+
+    if(playerX > 280) playerX = 280;
+    if(playerY > 280) playerY = 280;
+
+    renderPlayer();
+}
+
+function renderPlayer(){
+
+    const player =
+        document.getElementById("player");
+
+    player.style.left =
+        playerX + "px";
+
+    player.style.top =
+        playerY + "px";
+}
+
+function near(x,y){
+
+    const dx = playerX - x;
+    const dy = playerY - y;
+
+    return Math.sqrt(
+        dx*dx + dy*dy
+    ) < 60;
+}
