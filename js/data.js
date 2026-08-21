b1b03faf5366126c32cc5b1b408d391b58c1978c
cuirenx-dot <cuirenx@gmail.com>
2026-08-21 16:32:50 +0900

    Update data.js

--- js/data.js
+++ js/data.js
@@ -1,1 +1,30 @@
+const END_NAMES = {
+    end1: "END1 さよなら",
+    end2: "END2 約束",
+    end3: "END3 忘却",
+    end4: "END4 見送り",
+    end5: "END5 白",
+    end6: "END6 最後の放課後",
+    end7: "END7 切符の行き先",
+    end8: "END8 繰り返し",
+    trueEnd: "TRUE END 発車の向こう側"
+};
 
+const DEFAULT_SAVE = {
+    scene: "title",
+
+    ticket: false,
+    promise: false,
+
+    endings: {
+        end1:false,
+        end2:false,
+        end3:false,
+        end4:false,
+        end5:false,
+        end6:false,
+        end7:false,
+        end8:false,
+        trueEnd:false
+    }
+};
