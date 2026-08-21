c293e69f0ebee63deb8e4d03e668819f69caab0b
cuirenx-dot <cuirenx@gmail.com>
2026-08-21 16:46:39 +0900

    Update events.js

--- js/events.js
+++ js/events.js
@@ -1,1 +1,81 @@
+const flags = {
 
+    ticket:false,
+
+    bench:false,
+
+    clock:false,
+
+    talkedYui:false
+};
+
+function interact(){
+
+    const map =
+        MAPS.station_day1;
+
+    for(const obj of map.objects){
+
+        if(near(obj.x,obj.y)){
+
+            runEvent(obj.id);
+
+            return;
+        }
+    }
+
+    showMessage(
+        "",
+        "何もない。"
+    );
+}
+
+function runEvent(id){
+
+    switch(id){
+
+        case "ticket":
+
+            flags.ticket = true;
+
+            showMessage(
+                "切符",
+                "古い切符を拾った。"
+            );
+
+            break;
+
+        case "bench":
+
+            flags.bench = true;
+
+            showMessage(
+                "ベンチ",
+                "H+Yと刻まれている。"
+            );
+
+            break;
+
+        case "clock":
+
+            flags.clock = true;
+
+            showMessage(
+                "時計",
+                "13:13で止まっている。"
+            );
+
+            break;
+
+        case "yui":
+
+            flags.talkedYui = true;
+
+            showMessage(
+                "ユイ",
+                "発車まで、あと三日だね。"
+            );
+
+            break;
+    }
+}
